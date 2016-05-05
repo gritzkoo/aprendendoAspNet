6d4a971d1730a4bc5a7cfd26a132c51d7b686d3d
Gritzko Daniel Kleiner <gritzkoo@hotmail.com>
2016-05-05 14:01:50 -0300

    Inicio do projeto de aprendizado mvc

---
+++ phdtreinamentos/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace phdtreinamentos.Controllers
+{
+    public class HomeController
+    {
+        public Action Index()
+        {
+            return View();
+        }
+    }
+}
