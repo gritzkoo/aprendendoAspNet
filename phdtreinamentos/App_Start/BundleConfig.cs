55e63dd84a6872d57e46b31b24c77c4449c97b18
Gritzko Daniel Kleiner <gritzkoo@hotmail.com>
2016-05-10 00:17:45 -0300

    inclusão do bootstrap, jquery, knockout, system.web.optimization

---
+++ phdtreinamentos/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace phdtreinamentos.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles (BundleCollection bundle)
+        {
+            bundle.Add(new StyleBundle("~/css/bootstrap").Include("~/Content/bootstrap.css"));
+            bundle.Add(new ScriptBundle("~/js/bootstrap").Include("~/Scripts/bootstrap.js"));
+            bundle.Add(new ScriptBundle("~/js/jquery").Include("~/Scripts/jquery-1.9.1.js"));
+            bundle.Add(new ScriptBundle("/js/knockoutjs").Include("~/Scripts/knockout-{vertion}.js", "~/Scripts/knockout.validation.min.js"));
+        }
+    }
+}
